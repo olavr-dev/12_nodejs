93d44d1e74e52cc80546c0daf13c727f8c86d06b
Olav Rorvik <olavr.dev@gmail.com>
2024-04-03 12:19:33 +0200

    starting a server with nodejs

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const http = require('http');
 
-const server = http.createServer();
+function handleRequest(request, response) {
+  // localhost:3000/currenttime
+  if (request.url === '/currenttime') {
+    response.statusCode = 200;
+    response.end('<h1>' + new Date().toISOString() + '</h1>');
+    // localhost:3000
+  } else if (request.url === '/') {
+    response.statusCode = 200;
+    response.end('<h1>Hello, world!</h1>');
+  }
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(3000);
